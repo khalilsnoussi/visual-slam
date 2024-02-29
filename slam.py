5230003b23bc10f7a356a185597144515ac6a4ff
Khalil Snoussi <136627481+khalilsnoussi@users.noreply.github.com>
2024-02-28 20:39:25 -0500

    setting up the SLAM algorithm, orb detector

--- slam.py
+++ slam.py
@@ -4,18 +4,48 @@ import cv2 as cv
 
 
 
+class Frame():
+    def __init__(self, path : str, depth = False) -> None:
+        self.path = path
+        if depth:
+            self.img = cv.imread(self.path, cv.IMREAD_UNCHANGED)
+        else:
+            self.img = cv.imread(self.path)
+        
+        #calculating useful informations about the frame
+        if self.img != None:
+            self.height, self.width, self.channels = self.img.shape
+
+    #abreging the use of boilerplate code
+    def show(self) -> None:
+        if self.img != None:
+            cv.imshow(self.img)
+            cv.waitKey(0)
+            cv.destroyAllWindows()
+    
 
 class SLAM():
     
-    def __init__(self, frames, ) -> None:
-        pass
+    def __init__(self, frames : list) -> None:
+        """_summary_
+
+        Args:
+            frames (list): a list of Frames objects.
+        """
+        self.frames = self.frames
+        self.frame1 = self.frames[0] #Frame object
+        self.frame2 = self.frames[1] #Frame object
     
     
     
     def feature_extractor(self):
-        orb = cv.ORB()
+        orb = cv.ORB_create()
+        keypoints1, descriptors1 = orb.detectAndCompute(self.frame1.img, None)
+        keypoints2, descriptors2 = orb.detectAndCompute(self.frame2.img, None)
         pass
     
-    def bundle_adjustment(self):
+    def show(self):
         pass
     
+
+
