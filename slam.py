d8a04feb568710e01e9279152817ef94c8c9642a
Khalil Snoussi <136627481+khalilsnoussi@users.noreply.github.com>
2024-02-28 19:02:46 -0500

    setting up the environement

---
+++ slam.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2 as cv
+
+
+
+
+
+class SLAM():
+    
+    def __init__(self, frames, ) -> None:
+        pass
+    
+    
+    
+    def feature_extractor(self):
+        orb = cv.ORB()
+        pass
+    
+    def bundle_adjustment(self):
+        pass
+    
